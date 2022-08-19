403c6c70577d00c3f18b739ade3418930c38621c
buentellopc <buentellopc@com>
2022-08-19 16:49:00 -0500

    [step37] ManyToMany - customizing the join table

--- src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Student.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Student.java
@@ -7,55 +7,57 @@ import java.util.List;
 @Entity
 public class Student {
 
-	@Id
-	@GeneratedValue
-	private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
-	@Column(nullable = false)
-	private String name;
-	@OneToOne(fetch = FetchType.LAZY)
-	private Passport passport;
+    @Column(nullable = false)
+    private String name;
+    @OneToOne(fetch = FetchType.LAZY)
+    private Passport passport;
 
-	@ManyToMany
-	private List<Course> courses =new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "STUDENT_COURSE", joinColumns = @JoinColumn(name = "STUDENT_ID"), inverseJoinColumns =
+    @JoinColumn(name = "COURSE_ID"))
+    private List<Course> courses = new ArrayList<>();
 
-	protected Student() {
-	}
+    protected Student() {
+    }
 
-	public Student(String name) {
-		this.name = name;
-	}
+    public Student(String name) {
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Passport getPassport() {
-		return passport;
-	}
+    public Passport getPassport() {
+        return passport;
+    }
 
-	public void setPassport(Passport passport) {
-		this.passport = passport;
-	}
+    public void setPassport(Passport passport) {
+        this.passport = passport;
+    }
 
-	public List<Course> getCourses() {
-		return courses;
-	}
+    public List<Course> getCourses() {
+        return courses;
+    }
 
-	public void addCourse(Course course) {
-		this.courses.add(course);
-	}
+    public void addCourse(Course course) {
+        this.courses.add(course);
+    }
 
-	@Override
-	public String toString() {
-		return String.format("Student[%s]", name);
-	}
+    @Override
+    public String toString() {
+        return String.format("Student[%s]", name);
+    }
 }
